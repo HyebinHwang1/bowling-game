12a1ba2b834ed9c4589ab34af010f25dbba0c364
HyebinHwang1 <medi.bin@mediquitous.com>
2025-05-15 10:30:43 +0900

    Chore: vitest 타입에러로 인해, vite 버전 변경

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
   plugins: [react(), tailwindcss()],
-});
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+  },
+}));
